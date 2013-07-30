2fd6581ddf7c746f6ffaf3337aeee8f532435049
Carl Jackson <linkedin@carljackson.com>
2013-07-29 20:38:24 -0700

    firsties

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkedinOauth2Test::Application.config.secret_token = '2dd0fb5a03db116877266b8630927401369967fb87c6edbd7c02d872a59721bc52f792edaf338a582c27f759b3fa7e71a1678a919c093e683dbffa487c7227e2'
